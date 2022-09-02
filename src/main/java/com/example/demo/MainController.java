f21fc1e2910448e07a445e5c41374642d6563676
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-09-02 21:54:19 +0100

    add inputs variables

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -2,10 +2,15 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    private TextField txtName;
+    private TextField txtEmail;
+    private TextField txtMobile;
+    private TextField txtCourse;
+
 
     @FXML
     protected void Add() {
