568426f555eaaea4b41db74ba1ebb2bc5c1f302f
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-09-03 00:14:51 +0100

    getters and setters

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -17,4 +17,44 @@ public class Student {
         mobile = new SimpleStringProperty(this, "mobile");
         course = new SimpleStringProperty(this, "course");
     }
+
+    public StringProperty getId() {
+        return id;
+    }
+
+    public StringProperty getName() {
+        return name;
+    }
+
+    public StringProperty getEmail() {
+        return email;
+    }
+
+    public StringProperty getMobile() {
+        return mobile;
+    }
+
+    public StringProperty getCourse() {
+        return course;
+    }
+
+    public void setId(String id) {
+        this.id.set(id);
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile.set(mobile);
+    }
+
+    public void setCourse(String course) {
+        this.course.set(course);
+    }
 }
